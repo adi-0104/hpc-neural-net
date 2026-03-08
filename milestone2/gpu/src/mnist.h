7fcd73c52d704e805bb7d986eae9f2c5c0ad2909
Adithya <adithyasuresh.ak@gmail.com>
2026-03-08 11:40:55 -0500

    add c link

--- milestone2/gpu/src/mnist.h
+++ milestone2/gpu/src/mnist.h
@@ -8,5 +8,13 @@ typedef struct
     int n_cols;
 } MNISTData;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 MNISTData load_mnist(const char *image_path, const char *label_path);
 void free_mnist(MNISTData *data);
+
+#ifdef __cplusplus
+}
+#endif
